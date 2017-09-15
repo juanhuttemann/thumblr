36619041fc557e266f1d1a9684b6a633e40fd3a5
Juan Huttemann <juanfhuttemann@gmail.com>
2017-09-15 12:42:36 -0400

    Public Assets and Devise Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'welcome/index'
+
+  authenticated :user do
+    root 'posts#index', as: 'authenticated_root'
+  end
+
+  root 'welcome#index'
   resources :posts
-  root "posts#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912001616) do
+ActiveRecord::Schema.define(version: 20170912002731) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
